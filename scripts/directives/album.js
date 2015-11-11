287011b5f37cd2ee6b5e60045290199caff8d75f
alberto barquin <albertobarquin@iMac-de-alberto-barquin.local>
2015-11-11 08:24:14 +0100

    comienzo el refactor para pasar la vista del td de albums a una directiva

--- scripts/directives/album.js
+++ scripts/directives/album.js
@@ -1,8 +1,11 @@
 angular.module('Jeviteca').directive('album',function(){
     return{
+        restrict: "A",
         templateUrl:"views/album.html",
         scope: {
             album: "="
         }
     }
 });
+//TODO implementar on click para ir a la vista de detalle
+//TODO implementar el botón de añadir a favoritos
